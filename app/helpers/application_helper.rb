2359536c9a9e15f4300e5f69f0925b5a55aad12c
Robert Don <rdon05@gmail.com>
2013-11-21 19:58:56 -0600

    Layout Commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
-
 #Returns the full title on a per-page basis
-don
   def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
